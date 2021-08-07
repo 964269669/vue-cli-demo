21c6801aefd870a4ad15f339f4b0d213fca50b4c
964269669 <964269669@qq.com>
2021-08-07 22:54:16 +0800

    安装elementui,配置主题及常用变量

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
-    我的教育
+    大家好
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
     <router-view />
   </div>
 </template>
-<script>
+// 需要加 lang="ts"
+<script lang="ts">
+import Vue from 'vue'
 
-export default {
-  components: {
-
+export default Vue.extend({
+  data () {
+    return {
+      a: 1,
+      b: '2'
+    }
+  },
+  methods: {
+    test () {
+      // this.b.abc()
+    }
   }
-}
+})
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
---
+++ src/App222.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    我的教育
+    <h1 @click="d">{{a}}</h1>
+    <router-view />
+  </div>
+</template>
+// 需要加 lang="ts"
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+// 类的装饰器，用来修饰下面的类；
+@Component
+export default class App extends Vue {
+  // classAPI是当前组件的name属性
+  a = 1
+  b = '2'
+  c = {
+    a: 1, b: '2'
+  }
+
+  d () {
+    console.log(this.a)
+    // this.b.abc()
+  }
+
+  get name () {
+    return this.a + ' ' + this.b
+  }
+
+  set name (value) {
+    this.b = value + 2
+  }
+
+  mounted () {
+    console.log('mounted')
+    setTimeout(() => {
+      console.log(this.name)
+    }, 1000)
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
