c09faac1cfc3eeff8be53aead899be06b41e4e3d
964269669 <964269669@qq.com>
2021-08-08 10:32:19 +0800

    配置跨域代理

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 // vue.config.js
 module.exports = {
+  devServer: {
+    proxy: {
+      '/boss': {
+        target: 'http://eduboss.lagou.com',
+        changeOrigin: true // 把请求头中的 host 配置为 target
+      },
+      '/front': {
+        target: 'http://edufront.lagou.com',
+        changeOrigin: true
+      }
+    }
+  },
   css: {
     loaderOptions: {
       // 默认情况下 `sass` 选项会同时对 `sass` 和 `scss` 语法同时生效
